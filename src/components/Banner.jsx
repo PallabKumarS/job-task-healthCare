070270c765e55ef0a9ccef22a3c0d58701be4898
PallabKumarS <pallabkumar26@gmail.com>
2024-07-04 03:16:48 +0600

    fixed some issues

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,9 @@
 import banner from "../assets/Rectangle 5.png";
-import bannerBG from "../assets/image_bg.png";
+// import bannerBG from "../assets/image_bg.png";
 
 const Banner = () => {
   return (
-    <section className="mx-4 relative my-24">
-      <div className="">
-        {/* <div className="flex">
-          <img src={bannerBG} className="w-full" alt="banner" />
-        </div>
-        <div className="">
-          <img src={bannerBG} className="w-full " alt="banner" />
-        </div> */}
-      </div>
+    <section className="mx-4 my-24">
       <div className=" lg:mx-48">
         <img src={banner} className="w-full mb-8" alt="banner" />
       </div>
