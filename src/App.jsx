5b2690b5a7ed442378b8fd0cfd69d04c1fabe190
PallabKumarS <pallabkumar26@gmail.com>
2024-07-03 18:04:39 +0600

    done with navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="bg-[#E8F7FF]">
+      {/* navbar here */}
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
